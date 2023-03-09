d49b78c5982ba9003c4f182f824eb9d9d1cf0de6
Marquis4 <marquistumey12@gmail.com>
2023-03-09 08:45:04 -0800

    conversion

---
+++ printf.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int num = 71
+printf("The number is %d\n", num);  // output: The number is 71
+printf("The number is %i\n", num);  // output: The number is 71
