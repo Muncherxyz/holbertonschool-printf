cc7f3a5731a0b78acc956e3834cc150d82b32e46
Muncherxyz <5532@holbertonstudents.com>
2023-03-11 06:53:26 -0800

    updatedHeaderFile

--- main.h
+++ main.h
@@ -7,11 +7,30 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/* Documentations */
+
+/**
+ * struct format - a struct that holds va_list
+ * @let: character
+ * @func: pointer to a function
+ *
+ * Return: 0 success
+*/
+
+typedef struct format
+{
+	char *let;
+	int (*func)(va_list ap);
+} format_t;
 
 
 
 int _printf(const char *format, ...);
 int cti_function(va_list ap, char c);
+int print_char(va_list ap);
+int print_str(va_list ap);
+int print_int(va_list ap);
+int print_unsigned_int(unsigned int n);
 int _putchar(char c);
 
 #endif 
