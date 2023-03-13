4ab516b95a1119a4661b192992a7bad219a8ad61
Muncherxyz <5532@holbertonstudents.com>
2023-03-13 06:46:36 -0700

    fix

--- main.c
+++ main.c
@@ -3,16 +3,9 @@
 
 int main(void)
 {
-<<<<<<< HEAD
-  int retVal;
+
+int retVal;
 retVal = _printf("%s", NULL);
 printf("\n%d\n", retVal);
-=======
-int retVal, retVal2;
-retVal = _printf("%s", NULL);
-retVal2 = printf("%s", NULL);
-printf("\n%d%d\n", retVal, retVal2);
->>>>>>> master
-
 return (0);
 }
