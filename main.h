59b7589a61fb9eb2870f4dda45cef919d7dd21ce
Muncherxyz <5532@holbertonstudents.com>
2023-03-13 10:25:25 -0700

    fixeddeclarations

--- main.h
+++ main.h
@@ -19,7 +19,8 @@ int cti_function(va_list ap, char c);
 int print_char(va_list ap);
 int print_str(va_list ap);
 int print_int(va_list ap);
-int print_percent(va_list arg);
+int print_unsigned_int(unsigned int n);
+int _pow_recursion(int x, int y);
 int _putchar(char c);
 
 #endif
