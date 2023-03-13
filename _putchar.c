367004ea1c09e9e04baa72789566b2154ecb8a68
Muncherxyz <5532@holbertonstudents.com>
2023-03-13 06:02:14 -0700

    ..

--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include <unistd.h>
 /**
- * _putchar - is a function that writes the character c to the stdout.
- * @c: character to print
  *
- * Return: 1 is returned on success, -1 is given on error.
-*/
+ *
+ *
+ */
+
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+  return (write(1, &c, 1));
 }
--- main.h
+++ main.h
@@ -1,17 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
 #include <unistd.h>
 
 
-
-
-int _printf(const char *format, ...);
-int cti_function(va_list ap, char c);
 int _putchar(char c);
 
-#endif 
+#endif
