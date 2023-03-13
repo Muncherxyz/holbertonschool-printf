ab85de2c593115e20758f604a89c806bbab5976e
Muncherxyz <5532@holbertonstudents.com>
2023-03-13 10:00:11 -0700

    polishedmainandremovedsomeredudancy

--- _printf.c
+++ _printf.c
@@ -3,36 +3,43 @@
 * _printf - function that takes in a string [redacted]
 *
 */
-int _printf(const char *format, ...)
-{
-	va_list ap;
-	int i = 0, charCount = 0;
+int _printf(const char *format, ...) {
+    va_list args;
+    int i, charCount = 0;
 
-	if (format == NULL || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	va_start(ap, format);
-	for (i = 0; format[i]; i++)
-	{
-		if (format[i] == '%' && (format[i + 1] == 0 || format[i + 1] == '%'))
-		{
-			_putchar('%');
-			i++;
-			charCount++;
-		}
-		else if (format[i] == '%')
-		{
-			charCount += cti_function(ap, format[i + 1]);
-			i++;
-		}
-		else
-		{
-			charCount += 1;
-			_putchar(format[i]);
-		}
-	}
-	va_end(ap);
-	return (charCount);
+    if (format == NULL) {
+        return -1;
+    }
+
+    va_start(args, format);
+
+    for (i = 0; format[i]; i++) {
+        if (format[i] == '%') {
+            
+            i++;
+            if (format[i] == '%') {
+                
+                _putchar('%');
+                charCount++;
+            } else if (!format[i]) {
+                
+                va_end(args);
+                return -1;
+            } else {
+                
+               charCount += cti_function(args, format[i]);
+            }
+        } else {
+           
+            _putchar(format[i]);
+            charCount++;
+        }
+    }
+
+    va_end(args);
+    return charCount;
 }
+
 
 /**
 * cti_function - conversion type identifier
