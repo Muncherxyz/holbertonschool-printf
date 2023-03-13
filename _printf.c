e332640f86f088fc97ccb611f3172fe9456291c6
Muncherxyz <5532@holbertonstudents.com>
2023-03-13 07:52:32 -0700

    error13

--- _printf.c
+++ _printf.c
@@ -3,44 +3,35 @@
 * _printf - function that takes in a string [redacted]
 *
 */
-int _printf(const char *format, ...) {
-    va_list args;
-    int char_count = 0;
-    int i = 0;
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i = 0, charCount = 0;
 
-    
-    if (format == NULL || (format[i] == '%' && !format[i + 1])) {
-        return -1;
-    }
-
-    
-    va_start(args, format);
-    
-    for (i = 0; format[i]; i++) {
-       
-        if (format[i] == '%' && (format[i + 1] == 0 || format[i + 1] == '%')) {
-            putchar('%');  
-            i++;  
-            char_count++;
-        }
-       
-        else if (format[i] == '%') {
-            
-            char_count += cti_function(args, format[i + 1]);
-            i++;
-        }
-       
-        else {
-            putchar(format[i]);
-            char_count++;
-        }
-    }
-
-   
-    va_end(args);
-
-    
-    return char_count;
+	if (format == NULL || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	va_start(ap, format);
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%' && (format[i + 1] == 0 || format[i + 1] == '%'))
+		{
+			_putchar('%');
+			i++;
+			charCount++;
+		}
+		else if (format[i] == '%')
+		{
+			charCount += cti_function(ap, format[i + 1]);
+			i++;
+		}
+		else
+		{
+			charCount += 1;
+			_putchar(format[i]);
+		}
+	}
+	va_end(ap);
+	return (charCount);
 }
 
 /**
